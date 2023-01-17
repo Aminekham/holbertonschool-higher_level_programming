a2e8fb69511b41e143730675d7478cbed0d633aa
Aminekham <lkham0508@gmail.com>
2023-01-17 18:17:47 +0100

    figure out last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ x = number % 10
 if number < 0:
     x = -number % 10
     x = -x
-if x < 6 and x > 0:
-    print("Last digit of", number, "is", x, "and is less than 6 and not 0")
+if x == 0:
+    print("Last digit of", number, "is", x, "and is 0")
 elif x > 5:
     print("Last digit of", number, "is", x, "and is greater than 5")
 else:
-    print("Last digit of", number, "is", x, "and is 0")
+    print("Last digit of", number, "is", x, "and is less than 6 and not 0")
