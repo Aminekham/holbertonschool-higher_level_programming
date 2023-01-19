500d2b04c61743cd5bfb40244eabe19f970e6409
Aminekham <lkham0508@gmail.com>
2023-01-19 20:42:40 +0100

    last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    ns = str(number)
+    if number < 0:
+        n = -number
+    else:
+        n = number
+    ns = str(n)
     if ns.isnumeric():
         r = ns[len(ns) - 1]
         print("{}".format(r), end = "")
