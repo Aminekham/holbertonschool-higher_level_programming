23f843d79c402ed042008ef55fbf9a06b9262a03
Aminekham <lkham0508@gmail.com>
2023-01-20 22:53:14 +0100

    args in python

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("0 arguments.")
     else:
         print("{:d} arguments:".format(len(sys.argv)-1))
-        for i in range(len(sys.argv) - 1):
+        for i in range(1, len(sys.argv)):
             print("{:d}: {:s}".format(i, sys.argv[i]))
