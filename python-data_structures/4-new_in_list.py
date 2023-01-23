11a3204a1351a89959e10eb59b62c5016368fe9d
Aminekham <lkham0508@gmail.com>
2023-01-23 22:55:30 +0100

    create a new modified list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -5,6 +5,8 @@ def new_in_list(my_list, idx, element):
         return(newlist)
     elif idx > len(newlist):
         return(newlist)
-    else: 
+    else:
+        x = my_list[idx]
         newlist[idx] = element
+        my_list[idx] = x
         return(newlist)
