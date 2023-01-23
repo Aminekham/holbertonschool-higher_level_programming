f26cf8b360c0875b96c0da5d39ff662c9fe03378
Aminekham <lkham0508@gmail.com>
2023-01-23 23:30:42 +0100

    no c in any word

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    newstring = ""
-    for i in range(len(my_string)):
-        if my_string[i] == "c":
-            newstring[i] == ""
+    newstr = ""
+    for ch in (my_string):
+        if (ch == 'c' or ch == 'C'):
+            newstr+= ''
         else:
-            newstring[i] = my_string[i]
-    return(newstring)
+            newstr+= ch
+    return(newstr)
