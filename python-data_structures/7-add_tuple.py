42d5c6f8097867180e04bf11445ac0e6a49e61d9
Aminekham <lkham0508@gmail.com>
2023-01-24 11:50:48 +0100

    adding two tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     addlist=[]
-    for i in range(len(tuple_a)):
+    x = len(tuple_a)
+    if x > len(tuple_b):
+        x = len(tuple_b)
+    for i in range(x):
         addlist.append(tuple_a[i] + tuple_b[i])
     newtuple = tuple(addlist)
     return(newtuple)
