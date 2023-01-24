95a17616d9f54dac12525278c3d8a21f9a8d223e
Aminekham <lkham0508@gmail.com>
2023-01-24 12:50:46 +0100

    the max int in a listt

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return(None)
+    x = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > x:
+            x = my_list[i]
+    return(x)
