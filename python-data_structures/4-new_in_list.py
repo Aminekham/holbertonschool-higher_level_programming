a457d678657b5e09395bb17008e437cca7574694
Aminekham <lkham0508@gmail.com>
2023-01-24 11:05:22 +0100

    add a new element instead of an old one in a new list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    newlist = my_list
+    newlist = []
     if idx < 0 :
+        newlist = my_list
         return(newlist)
-    elif idx > len(newlist):
+    elif idx > len(my_list):
+        newlist = my_list
         return(newlist)
     else:
-        x = my_list[idx]
-        newlist[idx] = element
-        my_list[idx] = x
+        for i in range(len(my_list)):
+            if (i == idx):
+                newlist.append(element)
+            else:
+                newlist.append(my_list [i])
         return(newlist)
+
