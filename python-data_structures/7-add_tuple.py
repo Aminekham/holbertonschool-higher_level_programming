10e85d42379905913da29b4c5b87438ae41c8e05
Aminekham <lkham0508@gmail.com>
2023-01-24 12:16:26 +0100

    adding two tuples of two integers

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    addlist=[]
-    for i in range(2):
-        addlist.append(tuple_a[i] + tuple_b[i])
-    newtuple = tuple(addlist)
-    return(newtuple)
+    s ,s1 = len(tuple_a), len(tuple_b)
+    x,y,x1,y1 = 0, 0, 0, 0
+    if s > 0:
+        x = tuple_a[0]
+    if s > 1:
+        y = tuple_a[1]
+    if s1 > 0:
+        x1 = tuple_b[0]
+    if s1 > 1:
+        y1 = tuple_b[1]
+    return(x+x1, y+y1)
