91392d918dfa942236dab6dea133bee30b75d87f
Aminekham <lkham0508@gmail.com>
2023-01-25 18:00:22 +0100

    fixing the empty dict case

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     x = 0
     y = ""
-    if a_dictionary is None or a_dictionary = {}:
+    if a_dictionary is None or a_dictionary == {}:
         return(None)
     for i in a_dictionary.keys():
         if int(a_dictionary[i]) > x:
