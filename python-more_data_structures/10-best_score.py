6b21da5750b8301103f0cc10a782bca7a8798b66
Aminekham <lkham0508@gmail.com>
2023-01-25 17:58:30 +0100

    figuring out the biggest value in a dict

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    x = 0
+    y = ""
+    if a_dictionary is None:
+        return(None)
+    for i in a_dictionary.keys():
+        if int(a_dictionary[i]) > x:
+            x = int(a_dictionary[i])
+            y = i
+    return(y)
