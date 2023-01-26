e0d66fd7674972138ece84745f5229135a2eee5c
Aminekham <lkham0508@gmail.com>
2023-01-26 17:02:58 +0100

    fixing none condition

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return(None)
     f = sorted(a_dictionary)
     for i in f:
         print("{}: {}".format(i, a_dictionary[i]))
