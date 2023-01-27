00f7354da683bb874690f8e6710c7f2531b63b7f
Aminekham <lkham0508@gmail.com>
2023-01-27 11:50:20 +0100

    print a safe list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end = "")
+            c = c + 1
+    except:
+        print()
+    return(c)
