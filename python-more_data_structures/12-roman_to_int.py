f0481d329e15129dd5434408bfbfb8a42a15244d
Aminekham <lkham0508@gmail.com>
2023-01-27 22:02:14 +0100

    roman to integers

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if roman_string is None:
         return(0)
-    dic = {"I": 1, "V": 5, "X": 10 , "L": 50, "C": 100, "D": 500, "M": 1000}
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     r = 0
     i = 0
     while (i < len(roman_string)):
