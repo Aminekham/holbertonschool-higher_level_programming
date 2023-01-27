cd2678c62c23fa2911f3c831cb639d292224c510
Aminekham <lkham0508@gmail.com>
2023-01-27 12:00:48 +0100

    print a list safely

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     c = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end = "")
+            print("{}".format(my_list[i]), end="")
             c = c + 1
     except:
         pass
