72e50d6ae1e3920394b59094ec607ab4c9f8241b
Aminekham <lkham0508@gmail.com>
2023-02-01 19:05:11 +0100

    fixing the case of non integer

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""Define an square with size class"""
+
+
+class Square:
+    """Define a private attribute"""
+    __size = None
+
+    def __init__(self, size=0):
+        if type(size) != int :
+            raise TypeError ("size must be integer")
+        if size < 0:
+            raise ValueError ("size must be >= 0")
+        self.__size = size
