f84378d4ed4dec1c566e19c039b581d2de2c6d96
Aminekham <lkham0508@gmail.com>
2023-02-01 19:30:29 +0100

    define the property

---
+++ python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define an square with size class and functions"""
+
+
+class Square:
+    """Define a private attribute"""
+    __size = None
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def size(self):
+        return(self.__size)
+    
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return(self.__size ** 2)
