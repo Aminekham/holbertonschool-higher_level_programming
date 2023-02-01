788e4c0a936d0eca42517c583296163e234b0110
Aminekham <lkham0508@gmail.com>
2023-02-01 19:07:37 +0100

    fixing the case of non integer

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
 
     def __init__(self, size=0):
         if type(size) != int :
-            raise TypeError ("size must be integer")
+            raise TypeError ("size must be an integer")
         if size < 0:
             raise ValueError ("size must be >= 0")
         self.__size = size
