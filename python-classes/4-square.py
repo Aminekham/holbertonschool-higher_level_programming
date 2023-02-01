8d3b3c8494b1a42d4e0c453d86c9339201528e71
Aminekham <lkham0508@gmail.com>
2023-02-01 19:32:06 +0100

    define the property

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,9 +9,11 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
+    @property
     def size(self):
         return(self.__size)
-    
+
+    @size.setter
     def size(self, value):
         if type(value) != int:
             raise TypeError("size must be an integer")
