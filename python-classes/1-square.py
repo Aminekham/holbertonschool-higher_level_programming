d295d72e76bff0650298b178e31239947b82cb37
Aminekham <lkham0508@gmail.com>
2023-02-01 18:41:36 +0100

    define a private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define an square with size class"""
+
+
+class Square:
+    """Define a private attribute"""
+    __size = None
+    def __init__(self,size):
+        self.__size = size
+
