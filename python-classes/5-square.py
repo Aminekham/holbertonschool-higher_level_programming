7b7ac5ce507a84038b7cb151cc41cc6d53d15d62
Aminekham <lkham0508@gmail.com>
2023-02-01 23:11:18 +0100

    drawing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Define an square with size class and functions"""
+
+
+class Square:
+    """Define a private attribute"""
+    __size = None
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return(self.__size ** 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(1, self.__size ** 2 + 1):
+            if i % self.__size == 0:
+                print("#")
+            else:
+                print("#", end="")
