6d2f206a704f58a4237a06658b2ec2a088d44569
Aminekham <lkham0508@gmail.com>
2023-02-02 16:35:00 +0100

    first test using doctest

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
-This function is used to test the usage of doctest tool 
+This function is used to test the usage of doctest tool
 Testing cases errors : -a or b are not integers
                        -floats not casting to integers
                        -a or b are extremly large i.e : 1e100, 1e50, 1e10
                        -...
 """
+
+
 def add_integer(a, b=98):
     """
-    add_integer function : function used to test the doctest with an 
+    add_integer function : function used to test the doctest with an
     externel .txt file in the tests sub_directory
-
     """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    a, b= int(a), int (b)
+    a, b = int(a), int(b)
     return(a + b)
