d806d324c434c2fae8916c20107da1b91bd1c933
Aminekham <lkham0508@gmail.com>
2023-02-03 11:40:50 +0100

    defining the property

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    writing the initial rectangle class
+"""
+
+
+class Rectangle:
+    """
+        The class defining a rectangle
+    """
+    def __init__(self, height=0, width=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    @property
+    def height(self):
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    
+
+
+    
