27a3a3c889aa2b7ae405a3d975b0d289154abbc9
Aminekham <lkham0508@gmail.com>
2023-02-03 18:16:18 +0100

    text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    a module to check for . ? and : and print two new lines when it finds them
+"""
+
+
+def text_indentation(text):
+    """
+        checking for ., ? and : while printing the text
+    """
+    for i in range(len(text)):
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            print()
+            print()
+        else:
+            print(text[i], end="")
