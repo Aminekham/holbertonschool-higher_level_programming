b7f66f141ba42a44f7292550c642956f4fe9ecea
Aminekham <lkham0508@gmail.com>
2023-02-03 12:02:16 +0100

    fixing a perimeter error

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -35,10 +35,10 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     def area(self):
         return(self.__height * self.__width)
-    
+
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return(0)
