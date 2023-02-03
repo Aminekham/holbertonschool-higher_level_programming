d78c35e67360525cfb201ecb91e0c38a192d2199
Aminekham <lkham0508@gmail.com>
2023-02-03 11:44:33 +0100

    defining the property

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     """
         The class defining a rectangle
     """
-    def __init__(self, height=0, width=0):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
