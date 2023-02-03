d29450f02b272ec0b46a87c2ffe668f43e5d9989
Aminekham <lkham0508@gmail.com>
2023-02-03 11:48:28 +0100

    defining the property

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         return(self.__height)
@@ -35,8 +35,4 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    
-
-
     
