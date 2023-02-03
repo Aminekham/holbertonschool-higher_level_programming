178d1dc683014d3d304058964fcb5dd0e1079553
Aminekham <lkham0508@gmail.com>
2023-02-03 14:40:43 +0100

    the delete magic method

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,10 +9,11 @@ class Rectangle:
         The class defining a rectangle
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        Rectangle.number_of_instances+= 1 
+        Rectangle.number_of_instances += 1 
 
     @property
     def width(self):
@@ -64,4 +65,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        Rectangle.number_of_instances-= 1
+        Rectangle.number_of_instances -= 1
