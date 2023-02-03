2e99a6f72dcac601bbf61e7b217eedacbced92c5
Aminekham <lkham0508@gmail.com>
2023-02-03 17:41:32 +0100

    say my name test cases and documentation fixed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,13 +3,13 @@
 a module for saying the name of a person
 while testing certain conditions
 """
+
+
 def say_my_name(first_name="", last_name=""):
     """
         the first and last name will be checked to confirm that their
         type is string
     """
-
-
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
