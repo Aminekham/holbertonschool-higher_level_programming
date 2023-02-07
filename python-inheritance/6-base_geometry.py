ac96db86dd5ee70d25999efb68970ef645dec2c7
Aminekham <lkham0508@gmail.com>
2023-02-07 13:32:13 +0100

    the area method created

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+we started with an empty method
+"""
+
+
+class BaseGeometry:
+    """
+    still working on the area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
