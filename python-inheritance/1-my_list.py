7a8dcdcf9e069d5f35a1db26bd5bbec0dbc55e81
Aminekham <lkham0508@gmail.com>
2023-02-07 11:18:32 +0100

    documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module prints a sorted list
+"""
+
+
 class MyList(list):
+    """
+    using the inheritence
+    """
     def print_sorted(self):
         print(sorted(self))
