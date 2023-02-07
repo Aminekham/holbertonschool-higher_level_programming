4f201014c543082706bba19afc19a55befe3a424
Aminekham <lkham0508@gmail.com>
2023-02-07 13:14:20 +0100

    checking if its continued in the class

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,7 +6,7 @@ this module is checking if the obj is an instance of the given class
 
 def is_kind_of_class(obj, a_class):
     """
-        using isinstance 
+        using isinstance
     """
     if isinstance(obj, a_class):
         return(True)
