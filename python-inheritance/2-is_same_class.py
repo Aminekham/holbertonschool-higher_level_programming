8758d457eed58d6bea3c66b9506cb917dfb6463d
Aminekham <lkham0508@gmail.com>
2023-02-07 12:50:10 +0100

    checking if the type is the needed class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,7 +8,7 @@ def is_same_class(obj, a_class):
     """
         checking using isinstance
     """
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return(True)
     else:
         return(False)
