945ae8738fcf715b02e9b183a204d4939302acfe
Aminekham <lkham0508@gmail.com>
2023-02-07 12:41:21 +0100

    knowing if the object is an instance of that class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+this module tells if the object 
+"""
+
+
 def is_same_class(obj, a_class):
+    """
+        checking using isinstance
+    """
     if isinstance(obj, a_class):
         return(True)
     else:
