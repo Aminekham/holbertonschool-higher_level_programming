61bfba652418095fd780e7325137b48a2ce0d72a
Aminekham <lkham0508@gmail.com>
2023-02-08 17:54:05 +0100

    full rectrangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -17,7 +17,9 @@ class Rectangle(base):
         self.integer_validator("height", height)
         self.__height = height
         self.__width = width
+
     def area(self):
         return(self.__height * self.__width)
+
     def __str__(self):
         return(f"[Rectangle] {self.__width}/{self.__height}")
