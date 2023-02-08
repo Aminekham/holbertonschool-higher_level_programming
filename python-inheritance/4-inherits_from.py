9b825a141ace349ac03ffc6bd43f229ec8610d30
Aminekham <lkham0508@gmail.com>
2023-02-08 18:10:45 +0100

    fixing the type of the object

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,7 +8,7 @@ def inherits_from(obj, a_class):
     """
     using the issubclass procedure
     """
-    if issubclass(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) != a_class:
         return(True)
     else:
         return(False)
