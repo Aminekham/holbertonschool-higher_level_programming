06255d13d94b859c817fdfe6d0da31924bf6e002
Aminekham <lkham0508@gmail.com>
2023-02-08 17:44:06 +0100

    the equal 0 error

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,5 +14,5 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         if type(value) != int:
             raise TypeError(f"{name} must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
