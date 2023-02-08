0816ea40afbf608552cac35039c7db3f3deef093
Aminekham <lkham0508@gmail.com>
2023-02-08 18:03:15 +0100

    the square inhereted from the rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The square class inhereted from the rectangle one
+"""
+
+
+rect = __import__('9-rectangle').Rectangle
+
+
+class Square(rect):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self._Rectangle__width = size
+        self._Rectangle__height = size
+        self.__size = size
+    def area(self):
+        return(self.__size ** 2)
