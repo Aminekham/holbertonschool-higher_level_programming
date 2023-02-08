c3bf38d8bef6ce036652e4cfd893736e993dddde
Aminekham <lkham0508@gmail.com>
2023-02-08 17:32:50 +0100

    the integreted validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+we started with an empty method
+"""
+
+
+class BaseGeometry:
+    """
+    still working on the area
+    adding the validation function
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value < 0:
+            raise ValueError(f"{name} must be greater than 0")
