e08cfff9fefa568f94f7f27177b19db0bbc96ff8
Aminekham <lkham0508@gmail.com>
2023-02-09 17:40:00 +0100

    write through files

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module writes into a file
+"""
+def write_file(filename="", text=""):
+    """
+    using the write file method
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+    return(len(text))
