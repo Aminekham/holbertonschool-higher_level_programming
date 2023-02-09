b03a294c7c5b741a22b95a02b4439d947ec4f3bc
Aminekham <lkham0508@gmail.com>
2023-02-09 17:55:28 +0100

    the json repr

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module gives the json representation
+"""
+
+
+def to_json_string(my_obj):
+    """
+    using the json modules to dump an object into the json format
+    """
+    import json
+    return(json.dumps(my_obj))
+3   
