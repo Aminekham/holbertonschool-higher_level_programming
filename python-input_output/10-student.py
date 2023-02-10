5ef26bb1e5a47f132421defc115221800e4bc5f6
Aminekham <lkham0508@gmail.com>
2023-02-10 18:04:05 +0100

    u can specify the keys u want

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -14,7 +14,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs = None):
+    def to_json(self, attrs=None):
         if attrs is not None:
             dic = dict()
             for i in attrs:
