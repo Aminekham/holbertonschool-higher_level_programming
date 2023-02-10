ad6fe0d014d84544edc2fcc5b92b2d15ef7aa198
Aminekham <lkham0508@gmail.com>
2023-02-10 17:36:09 +0100

    adding as a list to a json file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script adds the arguments from command line to a list and saves a json format
+"""
+
+import sys
+import json
+import os
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv
+filename = 'add_item.json'
+with open(filename, 'a+', encoding="utf-8") as f:
+    if os.path.getsize(filename) != 0:
+        items = load_from_json_file(filename)
+        items.extend(args[1:])
+        save_to_json_file(items, filename)
+    else:
+        items = []
+        items.extend(args[1:])
+        save_to_json_file(items, filename)
