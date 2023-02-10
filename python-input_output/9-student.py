f5b377622347a149d1a9333333a87c6459d698fa
Aminekham <lkham0508@gmail.com>
2023-02-10 17:55:05 +0100

    gives a json repr of the student class

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -8,9 +8,11 @@ class Student:
     first_name = ""
     last_name = ""
     age = 0
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+    
     def to_json(self):
         return(self.__dict__)
