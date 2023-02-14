be14875cbded9cee83599730f53b10d19d24fd67
Aminekham <lkham0508@gmail.com>
2023-02-14 17:28:23 +0100

    building the first rectangles

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+
+from models.base import Base
+class Rectangle(Base):
+    __width = 0
+    __height = 0
+    __x = 0
+    __y = 0
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+    @property
+    def width(self):
+        return(self.__width)
+    @width.setter
+    def width(self, value):
+        if type(value) == int and value > 0:
+            self.__width = value
+    @property
+    def height(self):
+        return(self.__height)
+    @height.setter
+    def height(self, value):
+        if type(value) == int and value > 0:
+            self.__height = value
+    @property
+    def x(self):
+        return(self.__x)
+    @x.setter
+    def x(self, value):
+        if type(value) == int and value > 0:
+            self.__x = value
+    @property
+    def y(self):
+        return(self.__y)
+    @y.setter
+    def y(self, value):
+        if type(value) == int and value > 0:
+            self.__y = value
