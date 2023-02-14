f936fbd711403dd5d673844aca3bd6c6b33b3d51
Aminekham <lkham0508@gmail.com>
2023-02-14 22:40:49 +0100

    the str magic method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,5 +88,7 @@ class Rectangle(Base):
                     print("#")
                 else:
                     print("#", end="")
+
     def __str__(self):
-        return(f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
+        save = f"{self.__width}/{self.__height}"
+        return(f"[Rectangle] ({self.id}) {self.__x}/{self.__y} -{save}")
