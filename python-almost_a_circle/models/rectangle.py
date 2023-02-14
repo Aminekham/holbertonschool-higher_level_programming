74fcbdacb2422bc808b8b3193fb74d54f02e527a
Aminekham <lkham0508@gmail.com>
2023-02-14 17:30:41 +0100

    documented

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """
-
+This module represents the rectangle in our new base
 """
 
 
 from models.base import Base
 class Rectangle(Base):
+    """
+    These are the public attributes of the rectangle
+    """
     __width = 0
     __height = 0
     __x = 0
     __y = 0
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        The init magic method for our class to set the value and use the inheretance feature
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+
     @property
     def width(self):
         return(self.__width)
@@ -23,6 +31,7 @@ class Rectangle(Base):
     def width(self, value):
         if type(value) == int and value > 0:
             self.__width = value
+
     @property
     def height(self):
         return(self.__height)
@@ -30,6 +39,7 @@ class Rectangle(Base):
     def height(self, value):
         if type(value) == int and value > 0:
             self.__height = value
+
     @property
     def x(self):
         return(self.__x)
@@ -37,6 +47,7 @@ class Rectangle(Base):
     def x(self, value):
         if type(value) == int and value > 0:
             self.__x = value
+
     @property
     def y(self):
         return(self.__y)
