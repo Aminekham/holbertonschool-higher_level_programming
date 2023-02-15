1f4eb4fb3bb28e21cc09a5ee0af6e2643163c48e
Aminekham <lkham0508@gmail.com>
2023-02-16 00:10:38 +0100

    giving the json representation of a list dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,3 +16,12 @@ class Base:
             self.id = id
         else:
             self.id = Base.__nb_objects
+    
+    def to_json_string(list_dictionaries):
+        """
+        This method returns the json representation of list_dictionaries variable
+        """
+        import json
+        if list_dictionaries is None or list_dictionaries == []:
+            return("[]")
+        return(json.dumps(list_dictionaries))
