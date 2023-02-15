a72f4c719b26c368a49cdc07205cb45f3b189fcb
Aminekham <lkham0508@gmail.com>
2023-02-15 22:28:29 +0100

    the documentation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-
+This model represents our created square
 """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    The init function and the string representation
+    """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
     def __str__(self):
