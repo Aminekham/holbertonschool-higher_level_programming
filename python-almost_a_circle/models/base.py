e9f65584189f786e30a0e87e43aa5e59cdc44fd1
Aminekham <lkham0508@gmail.com>
2023-02-16 17:45:40 +0100

    saving the json format into a file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,3 +26,21 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return("[]")
         return(json.dumps(list_dictionaries))
+
+    def save_to_file(cls, list_objs):
+        """
+        The method to save the json format to a .json file
+        """
+        import json
+        if list_objs is None:
+            list_objs = []
+        with open(f"{cls.__name__}.json", "w") as f:
+            list_dic = []
+            for i in range(len(list_objs)):
+                x = list_objs[i]
+                d = x.to_dictionary()
+                list_dic.append(d)
+            print(list_dic)
+            s = cls.to_json_string(list_dic)
+            print(s)
+            json.dump(s, f)
