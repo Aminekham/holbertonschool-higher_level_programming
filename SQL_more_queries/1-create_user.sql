55fb7d3887c73c19aab90dbccf25d9d02e04d9f5
Aminekham <lkham0508@gmail.com>
2023-03-15 23:06:07 +0100

    fixing the code

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Giving all privileges 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1';
-SET PASSWORD FOR 'user_0d_1' = PASSWORD('user_0d_1_pwd');
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
