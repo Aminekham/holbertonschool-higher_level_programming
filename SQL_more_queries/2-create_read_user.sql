84e2a478ff7462be8b62de1139f6ecce55a5521e
Aminekham <lkham0508@gmail.com>
2023-03-15 23:11:38 +0100

    create a user and a db

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- creating a database and a new user inside of it
 CREATE DATABASE hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
