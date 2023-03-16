6e2231c2c8fb24d4f3edf1ac9f8a4c1201af98f5
Aminekham <lkham0508@gmail.com>
2023-03-16 19:37:04 +0100

    finally python again

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,9 +4,11 @@ This module is used to list our mysql data using python
 """
 import MySQLdb
 import sys
-connect = MySQLdb.connect(host = "localhost", port = 3360, user = sys.argv[0], passwd = sys.argv[1], db = sys.argv[2])
-cursor = connect.cursor()
-cursor.excute("SELECT states.id FROM states ORDER BY states.id ASC")
-row = cursor.fetchall()
-for r in row:
-    print(r)
+
+if __name__ == "__main__":
+    connect = MySQLdb.connect(host = "localhost", port = 3360, user = sys.argv[0], passwd = sys.argv[1], db = sys.argv[2])
+    cursor = connect.cursor()
+    cursor.excute("SELECT * FROM states ORDER BY states.id ASC")
+    row = cursor.fetchall()
+    for r in row:
+        print(r)
