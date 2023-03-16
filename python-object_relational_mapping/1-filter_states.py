b7a25b1be5e171233c370993f1805b9c8681b20c
Aminekham <lkham0508@gmail.com>
2023-03-16 19:50:30 +0100

    filtering by name

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module is used to list our mysql data using python
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    connect = MySQLdb.connect(
+        host="localhost",
+        port=3360,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    row = cursor.fetchall()
+    for r in row:
+        if r[0] == "N":
+            print(r)
