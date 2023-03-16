96ee32a786422653d39d12cb8f8737868241ca1f
Aminekham <lkham0508@gmail.com>
2023-03-16 16:04:31 +0100

    create cities table linked to states table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,6 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY id.states,
-    name VARCHAR(256) NOT NULL
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );
