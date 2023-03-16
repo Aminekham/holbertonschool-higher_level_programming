de51ac6f56b5f0cb988db126d0ecfe2cddfaa853
Aminekham <lkham0508@gmail.com>
2023-03-16 19:44:25 +0100

    have no sql installed

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == "__main__":
     connect = MySQLdb.connect(host = "localhost", port = 3360, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     cursor = connect.cursor()
-    cursor.excute("SELECT * FROM states ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     row = cursor.fetchall()
     for r in row:
         print(r)
