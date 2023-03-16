8d5781c8447885c98f3a0662a34b635e4eed76fa
Aminekham <lkham0508@gmail.com>
2023-03-16 19:43:12 +0100

    fix argv

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    connect = MySQLdb.connect(host = "localhost", port = 3360, user = sys.argv[0], passwd = sys.argv[1], db = sys.argv[2])
+    connect = MySQLdb.connect(host = "localhost", port = 3360, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3])
     cursor = connect.cursor()
     cursor.excute("SELECT * FROM states ORDER BY states.id ASC")
     row = cursor.fetchall()
