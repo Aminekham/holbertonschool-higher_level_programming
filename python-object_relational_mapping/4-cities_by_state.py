328536af47ea36ebe4fa66d821cb9ba2c7f64240
Aminekham <lkham0508@gmail.com>
2023-03-17 13:41:43 +0100

    adding the state

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,8 +14,9 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-    cur=connect.cursor()
-    cur.execute("SELECT cities.id, cities.name,states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
-    inf=cur.fetchall()
+    cur = connect.cursor()
+    cur.execute("SELECT cities.id, cities.name,states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    inf = cur.fetchall()
     for i in inf:
         print(i)
