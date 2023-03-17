162df065a9a3c02921ba4170425a63ea889942b6
Aminekham <lkham0508@gmail.com>
2023-03-17 16:49:12 +0100

    fixing the comments

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """
 This module lists all the cities in a certain state using the state name
 """
+import MySQLdb
+import sys
+
+
 if __name__ == "__main__":
-    connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    connect = MySQLdb.connect(
+        host="localhost", 
+        port=3306, 
+        user=sys.argv[1], 
+        passwd=sys.argv[2], 
+        db=sys.argv[3]
+        )
     cur = connect.cursor()
     cur.execute("SELECT cities.id, cities.name,states.name FROM cities\
                 INNER JOIN states ON cities.state_id = \
@@ -13,4 +21,4 @@ if __name__ == "__main__":
     r = cur.fetchall()
     for i in r:
         if i[2] == sys.argv[4]:
-            print(format(i))
+            print(format(i[1]))
