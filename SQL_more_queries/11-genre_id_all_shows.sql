74f40d1ad9a7fe64c083db2d3c678a7f887f9f21
Aminekham <lkham0508@gmail.com>
2023-03-17 22:30:47 +0100

    using the right join

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- tv shows by genre ids
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows RIGHT JOIN tv_show_genres ON tv_shows.id= tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_shows.id= tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
