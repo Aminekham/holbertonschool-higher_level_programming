c6b0bf2c093944c67cca6abd4bea4c5dec0c32fc
Aminekham <lkham0508@gmail.com>
2023-03-17 13:18:56 +0100

    fixing the connect cursor

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-    cur=MySQLdb.cursor()
+    cur=connect.cursor()
     cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
     inf=cur.fetchall()
     for i in inf:
