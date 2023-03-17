7f19dfceb2555692b88e9cc087542e62241789bb
Aminekham <lkham0508@gmail.com>
2023-03-17 17:32:43 +0100

    the state model using sqlalchemy

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Write a python file that contains the class definition
-   of a State and an instance Base = declarative_base()"""
+"""
+using python to create our database
+including the sqlalchemy module
+"""
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
+import sys
 
 
 Base = declarative_base()
 
 
 class State(Base):
-    """class State"""
+    """
+    Declaring the tables as classes
+    """
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+Base.metadata.create_all(engine)
