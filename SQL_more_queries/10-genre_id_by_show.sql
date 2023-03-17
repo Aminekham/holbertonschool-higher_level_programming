5adc8226a9a1ab315371e48764e02dced99f1b1e
Aminekham <lkham0508@gmail.com>
2023-03-17 22:24:35 +0100

    tv show by genre id

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- tv shows by genre ids
-SELECT tv_show.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_show.id= tv_show_genres.show_id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_show.id= tv_show_genres.show_id
