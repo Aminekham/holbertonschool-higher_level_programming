6eebbc2813c1ba567b7b881395172084911f76b8
Aminekham <lkham0508@gmail.com>
2023-03-17 13:12:02 +0100

    cities

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+import MYSQLdb
+
+"""
+This module lists all the cities that we have in our database
+"""
+if __name__ == "__main__":
+    connect = MYSQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur=MYSQLdb.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    inf=cur.fetchall()
+    for i in inf:
+        print(i)
