0d99a020846875e823ef6c9305c6bc5bd7662047
Aminekham <lkham0508@gmail.com>
2023-03-17 17:54:20 +0100

    listing with sqlalchemy

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This script lists objects from database using sqlalchemy"""
+from model_state import Base, State
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql://%s:%s@localhost/%s", 
+                           sys.argv[0], sys.argv[1], sys.argv[2])
+    sess = sessionmaker()
+    sess.configure(bind=engine)
+    s = sess()
+    r = s.query(State).order_by(State.id).all()
+    for i in r:
+        print(f"{i.id}: {i.name}")
