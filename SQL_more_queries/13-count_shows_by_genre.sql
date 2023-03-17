addc7d2cb739ea204b0084888510dc2c9369933d
Aminekham <lkham0508@gmail.com>
2023-03-17 22:44:04 +0100

    counting how shows for each genre by joining and grouping

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- counting shows for each genre
 SELECT g.name AS genre, COUNT(*) AS number_of_shows
-FROM genres g
-JOIN shows_genres sg ON g.id = sg.genre_id
+FROM tv_genres g
+JOIN tv_shows_genres sg ON g.id = sg.genre_id
 GROUP BY g.id
 ORDER BY number_of_shows DESC;
