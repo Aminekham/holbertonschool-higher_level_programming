d657bdf6825a30083adbdb072a0bcc3d1db433aa
Aminekham <lkham0508@gmail.com>
2023-03-17 18:12:23 +0100

    listing with sqlalchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1],
         sys.argv[2],
-        sys.argv[3])
-        )
+        sys.argv[3]),
+        pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     # associate it with our custom Session class
     Session = sessionmaker()
 
+    # associate it with our custom Session class
     Session.configure(bind=engine)
 
     session = Session()
