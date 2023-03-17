2872fc14fbc049431941ba7b6ebc09655c084940
Aminekham <lkham0508@gmail.com>
2023-03-17 22:33:53 +0100

    filtering the no genre shows

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- tv shows by genre ids
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_shows.id= tv_show_genres.show_id WHERE tv_show_genres.genre_id = NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_shows.id= tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
