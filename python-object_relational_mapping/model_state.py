8d35be38a2146645ad05de9e5b500863ddeccf40
Aminekham <lkham0508@gmail.com>
2023-03-17 17:35:11 +0100

    fixing documentation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""using python to create our database
-including the sqlalchemy module"""
+"""using the sqlalchemy library to manipulate and create tables"""
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
-import sys
 
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-    Declaring the tables as classes
-    """
+    """The states table creation"""
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
-Base.metadata.create_all(engine)
