e32649ef51f6cdbf934eb5c695f7deba2667445b
Aminekham <lkham0508@gmail.com>
2023-04-20 18:32:57 +0200

    concat argvs

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
 
 const args = process.argv;
-if (args[2] == null) {
-  console.log("undefined", end = "");
-}else {
-  console.log(args[2], end ="");
-}
-console.log ("is", end = "");
-if (args[3] == null) {
-  console.log("undefined", end = "");
-}else {
-  console.log(args[3]);
-}
+
+console.log(args[2]," is ",args[3])
