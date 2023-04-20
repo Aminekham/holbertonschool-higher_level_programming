a7eed17831964543381e73243dd0197771c5b6a9
Aminekham <lkham0508@gmail.com>
2023-04-20 18:51:28 +0200

    figuring out if its int or not

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 const x = parseInt(args[2])
 if (Number.isInteger(x)) {
-    console.log(x);
+    console.log("My number:", x);
 }else {
     console.log("Not a number")
 }
