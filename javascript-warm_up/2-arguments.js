d12745a37fa58454f46bb5a59e50fff6ef35ea05
Aminekham <lkham0508@gmail.com>
2023-04-20 18:06:49 +0200

    trying the nodejs argv

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const args = process.argv
-if (args[2] == null){
-    console.log("No argument")
+if (args[2] == null) {
+    console.log("No argument");
 }
-else if (args[3] == null){
-    console.log("Argument found")
+else if (args[3] == null) {
+    console.log("Argument found");
 }
-else{
-    console.log("Arguments found")
+else {
+    console.log("Arguments found");
 }
