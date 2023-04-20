1cde202162510ca2eb9589f1670642002a895a13
Aminekham <lkham0508@gmail.com>
2023-04-20 21:12:05 +0200

    square fix

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 
 const args = process.argv;
 if (isNaN(args[2])) {
-  console.log("Missing size");
-}else {
+  console.log('Missing size');
+} else {
   for (let i = 0; i < parseInt(args[2]); i++) {
-  console.log("X".repeat(parseInt(args[2])));
+    console.log('X'.repeat(parseInt(args[2])));
   }
 }
