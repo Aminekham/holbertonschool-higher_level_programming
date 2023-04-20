b89d251bbc57e03c8368b90f64f0069aadf04775
Aminekham <lkham0508@gmail.com>
2023-04-20 19:23:38 +0200

    squares in node

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node 
 
 const args = process.argv;
-if (args[2] == null || Number.isInteger(args[2]) == false) {
+if (args[2] == null ) {
   console.log("Missing size");
+}else if (Number.isInteger(args[2]) == false) {
+  console.log("Missing size")
 }else {
   for (let i = 0; i < parseInt(args[2]); i++) {
     for(let i = 0; i < parseInt(args[2]); i++) {
