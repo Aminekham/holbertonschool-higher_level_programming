d9d69fc0411e409084ac581559bfa168a02cd8ba
Aminekham <lkham0508@gmail.com>
2023-04-22 00:11:37 +0200

    occ

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    let c = 0;
-    for (let i = 0; i < list.length; i++) {
-      if (list[i] === searchElement) {
-        c++;
-      }
+  let c = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      c++;
     }
-    return (c);
-  };
+   }
+   return (c);
+};
