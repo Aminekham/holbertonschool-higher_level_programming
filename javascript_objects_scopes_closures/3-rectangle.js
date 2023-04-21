221b879db8e71592c625304cd6cae39a7230c852
Aminekham <lkham0508@gmail.com>
2023-04-21 18:57:21 +0200

    fix style

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -4,12 +4,12 @@ class Rectangle {
   constructor (w, h) {
     if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
       return;
-  }
+    }
     this.width = w;
     this.height = h;
   }
 
-   print () {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
