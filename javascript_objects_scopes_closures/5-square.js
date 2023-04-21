ef0d0f0dd8f198290aff2c76fa3b5ce51d098b73
Aminekham <lkham0508@gmail.com>
2023-04-21 19:31:33 +0200

    first square

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -4,7 +4,8 @@ const Rectangle = require('./4-rectangle.js');
 
 class Square extends Rectangle {
   constructor(size) {
-    super(size);
+    super(w) = size;
+    super(h) = size;
   }
 }
 module.exports = Square;
