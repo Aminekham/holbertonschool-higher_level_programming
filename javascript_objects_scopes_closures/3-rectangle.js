18ee309322fbd27bdfe96c058445d58c3a66facd
Aminekham <lkham0508@gmail.com>
2023-04-21 18:55:43 +0200

    fix style

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -8,9 +8,10 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
-   print() {
-    for (let i = 0; i < this.height; i++){
-      console.log("X".repeat(this.width));
+
+   print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
