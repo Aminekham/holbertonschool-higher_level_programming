95f0125854c9bbe579ac52388f5be82bcd0bbf3e
Aminekham <lkham0508@gmail.com>
2023-04-21 18:22:59 +0200

    verif

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <=0) {
-      return(undefined);
-    }
-    else {
-      this.width = w;
-      this.height = h;
+    this.width = w;
+    this.height = h;
+    if (w <= 0 || h <= 0) {
+      this.width = undefined;
+      this.height = undefined;
     }
   }
 }
