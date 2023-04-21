a058c275249a7457e73e919249612a8fb50cd6fe
Aminekham <lkham0508@gmail.com>
2023-04-22 00:20:43 +0200

    fix

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   const l = [];
-  for (let i = list.length - 1; i >= 0; i--){
+  for (let i = list.length - 1; i >= 0; i--) {
     l.push(list[i]);
   }
   return (l);
