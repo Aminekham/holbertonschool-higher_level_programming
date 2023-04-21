85423ea4e70edf7afffa5875071afd0470dbc153
Aminekham <lkham0508@gmail.com>
2023-04-21 18:42:41 +0200

    fo

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,8 @@
 
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        return {};
+      if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
+        return;
       }
       this.width = w;
       this.height = h;
