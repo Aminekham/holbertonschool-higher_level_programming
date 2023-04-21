b3a4a0687f881b57dd8d20b37d5f9269c09987ca
Aminekham <lkham0508@gmail.com>
2023-04-21 03:32:02 +0200

    biggest second number

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,8 @@ const args = process.argv.slice(2);
 if (args[0] == null) {
   console.log(0);
 }
-j = parseInt(args[0])
+j = parseInt(args[0]);
+f = 0;
 for (let i = 1; i < args.lengh; i++) {
   if (j < parseInt(args[i])) {
     f = j;
