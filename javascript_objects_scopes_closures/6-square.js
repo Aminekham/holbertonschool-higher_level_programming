a3bbc15226cba657ff5760103a4e06956a23aa72
Aminekham <lkham0508@gmail.com>
2023-04-22 00:07:15 +0200

    c

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+
+const Rectangle = require('./5-square.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
