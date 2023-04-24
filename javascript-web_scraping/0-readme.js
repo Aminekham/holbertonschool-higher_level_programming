9f662884ac0365faa59dc841a75e918af757e29b
Aminekham <lkham0508@gmail.com>
2023-04-25 00:58:39 +0200

    fix styling

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const file_name = process.argv[2];
+const name = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(file_name, 'utf-8', (err, data) => {
+fs.readFile(name, 'utf-8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
