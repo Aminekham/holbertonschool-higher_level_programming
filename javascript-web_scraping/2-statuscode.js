2d163de6124ce1979179805ac5383ea03e2815ae
Aminekham <lkham0508@gmail.com>
2023-04-25 01:23:53 +0200

    fixing

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const link = process.argv[2];
+import fetch from 'node-fetch';
 
 async function getting_status(link) {
-  const fetch = require('node-fetch');
   const fetch_url = await fetch(link);
   console.log(fetch_url.status);
 }
