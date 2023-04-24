254a61202fd7510827e24b766d9f06cc36026f42
Aminekham <lkham0508@gmail.com>
2023-04-25 01:30:38 +0200

    working with request library instead of fetch

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const link = process.argv[2];
-const fetch = require('node-fetch');
-
-async function getting_status(link) {
-  const fetch_url = await fetch(link);
-  console.log(fetch_url.status);
-}
-
-getting_status(link);
+const request = require('request');
+request(link, function print_status(response){
+  console.log(response.statusCode);
+});
