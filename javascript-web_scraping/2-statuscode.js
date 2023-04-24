c43e8f69e6e9e7704035b714273a884eb3af5caf
Aminekham <lkham0508@gmail.com>
2023-04-25 01:40:38 +0200

    fixing output format

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const link = process.argv[2];
 const request = require('request');
-request(link, function print_status(error, response, body){
+request(link, function status (error, response, body) {
   if (error) {
     console.error(error);
   }
