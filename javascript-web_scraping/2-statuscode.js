62b8be20f262578fb103ca571f3e7c64daa6b491
Aminekham <lkham0508@gmail.com>
2023-04-25 01:25:23 +0200

    fixing

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const link = process.argv[2];
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
 async function getting_status(link) {
   const fetch_url = await fetch(link);
