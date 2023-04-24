fbce2cd6b89b2d9363d047b74afacc1c0637a0fb
Aminekham <lkham0508@gmail.com>
2023-04-25 01:56:33 +0200

    using the request module to deal with apis

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-id = process.agrv[2];
+const id = process.agrv[2];
 request('https://swapi-api.hbtn.io/api/films/'+id+'id', function title(error, response, body) {
   console.log(body);
 })
