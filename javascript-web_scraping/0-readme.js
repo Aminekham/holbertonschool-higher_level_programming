d17d85da95ce01bfe7164674e963e8e28b6f1d87
Aminekham <lkham0508@gmail.com>
2023-04-25 00:55:10 +0200

    reading a file in js

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+file_name = process.argv[2];
+const fs = require('fs');
+
+fs.readFile(file_name, 'utf-8' , (err, data) => {
+  if (err) throw err;
+  
+  console.log(data);
+
+})
