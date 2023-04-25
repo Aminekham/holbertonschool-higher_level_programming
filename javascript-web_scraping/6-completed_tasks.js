68642d4bce8b3dd0395544a1cad8ec54b0465799
Aminekham <lkham0508@gmail.com>
2023-04-25 13:40:27 +0200

    initializing the dict

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const link = process.argv[2];
+request(link, function(error, body, status) {
+  const parsed = JSON.parse(body);
+  let dict = {};
+  for(let j = 0; j < parsed.length; j++) {
+    x = parsed[j].userId;
+    if (dict[x] != 0){
+      dict[x] = 0;
+    }
+  }
+  console.log(dict);
+})
