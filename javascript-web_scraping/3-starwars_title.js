1036487f87913c84cd89d00ff89fe78d9f93183e
Aminekham <lkham0508@gmail.com>
2023-04-25 02:05:38 +0200

    fixing output

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,5 @@
 const request = require('request');
 const id = process.argv[2];
 request('https://swapi-api.hbtn.io/api/films/'+id, function title(error, response, body) {
-  console.log(body['title']);
+  console.log(body);
 })
