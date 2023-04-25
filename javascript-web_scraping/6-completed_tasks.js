8b387719bdbab1c1704ca93e6894e5564855ab8e
Aminekham <lkham0508@gmail.com>
2023-04-25 13:56:46 +0200

    initializing the dict

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -5,17 +5,17 @@ const apiUrl = process.argv[2];
 
 request(apiUrl, function (error, response, body) {
 
-    const tasks = JSON.parse(body);
-    const completedTasks = {};
-    for (let i = 0; i < tasks.length; i++) {
-      const task = tasks[i];
-      if (task.completed) {
-        if (task.userId in completedTasks) {
-          completedTasks[task.userId]++;
-        } else {
-          completedTasks[task.userId] = 1;
-        }
-      }
+  const tasks = JSON.parse(body);
+  const completedTasks = {};
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
+    if (task.completed) {
+      if (task.userId in completedTasks) {
+        completedTasks[task.userId]++;
+    } else {
+        completedTasks[task.userId] = 1;
     }
-    console.log(completedTasks);
+  }
+}
+  console.log(completedTasks);
   });
