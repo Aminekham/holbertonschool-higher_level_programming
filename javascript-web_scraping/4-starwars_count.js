44e99aaed300a08fc301e61fd3295df4f291a2ed
Aminekham <lkham0508@gmail.com>
2023-04-25 12:40:08 +0200

    counting

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -12,10 +12,9 @@ request(link, function (error, response, body) {
     return;
   }
   const parsed_json = JSON.parse(body);
-  l = [];
-  l.push(parsed_json);
+  console.log(parsed_json);
   for(let j = 0; j < body.length; j++){
-    const list = l[j][j].results;
+    const list = parsed_json.results;
     const chars = list[0].characters;
     let count = 0;
     for (let i=0; i < chars.length; i++) {
