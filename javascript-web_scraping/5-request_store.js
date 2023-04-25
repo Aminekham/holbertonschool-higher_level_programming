b7db305e1b873594a3f48b4ff110020578938c06
Aminekham <lkham0508@gmail.com>
2023-04-25 13:14:49 +0200

    getting the whole page content

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 const link = process.argv[2];
 const filename = process.argv[3];
 request(link, function(error, body, status){
-  fs.writeFile(filename, body, 'utf-8', err =>{
+  text = JSON.stringify(body)
+  fs.writeFile(filename, text, 'utf-8', err =>{
     if (err) throw err;
   });
 });
