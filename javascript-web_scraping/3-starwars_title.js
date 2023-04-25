b93770a0920b493808d1df0e00bd1ec0f0eae7a6
Aminekham <lkham0508@gmail.com>
2023-04-25 02:13:55 +0200

    parsing with json

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,6 +3,9 @@
 const request = require('request');
 const id = process.argv[2];
 request('https://swapi-api.hbtn.io/api/films/' + id, function title (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
   const parsed = JSON.parse(body);
   console.log(parsed["title"]);
-})
+});
