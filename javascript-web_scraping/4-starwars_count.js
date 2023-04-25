772d1d1d1527cb817646a453209a800675159d8f
Aminekham <lkham0508@gmail.com>
2023-04-25 12:27:19 +0200

    counting

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,15 +2,20 @@
 
 const request = require('request');
 const link = process.argv[2];
-request(link, function (error, body, status) {
-  if (error){
+request(link, function (error, response, body) {
+  if (error) {
     console.error(error);
+    return;
+  }
+  if (body.trim().length === 0) {
+    console.error('Empty response received');
+    return;
   }
   const parsed_json = JSON.parse(body);
   const list = parsed_json.results;
   const chars = list[0].characters;
   let count = 0;
-  for (let i; i < chars.length; i++) {
+  for (let i=0; i < chars.length; i++) {
     if (chars[i] == "https://swapi-api.hbtn.io/api/people/18/"){
       count = count + 1;
     }
