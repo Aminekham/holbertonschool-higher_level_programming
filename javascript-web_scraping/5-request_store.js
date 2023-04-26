4e98c33e650ee291ded01ed4a8d7534f2e1e13f7
Aminekham <lkham0508@gmail.com>
2023-04-26 13:09:56 +0200

    fixing

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,7 +4,7 @@ const request = require('request');
 const fs = require('fs');
 const link = process.argv[2];
 const filename = process.argv[3];
-request(link, function(error, response, body){
+request (link, function (error, response, body) {
   if (error) throw error;
   fs.writeFile (filename, body, 'utf-8', err => {
     if (err) throw err;
