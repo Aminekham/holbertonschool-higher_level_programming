d02f4e0acf0dd38ec9f0cb6ad82352251b56688c
Aminekham <lkham0508@gmail.com>
2023-04-28 18:01:16 +0200

    getting the character name

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+$(function () {
+    $.getJSON(
+      'https://swapi-api.hbtn.io/api/people/5/?format=json',function (data) {
+        $('DIV#character').text(data.name);
+      }
+    )
+  })
